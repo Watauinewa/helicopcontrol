a08e7805ab10b7a0c5f68611bea65509db77937e
jolt <mcubillosv@neotokyo4.com>
2019-09-11 18:31:57 -0300

    ++

--- src/math_tool/math_tool.cpp
+++ src/math_tool/math_tool.cpp
@@ -16,192 +16,185 @@ static double _ln_lf(double x);
 // (x debe ser siempre mayor que 0)
 static float _exp_f(float x) {
     const float root_e = 1.284025416687741;
-	const float a = 1.000000034750736;
-	const float b = 0.499997989964957;
-	const float c = 0.166704077809886;
-	const float d = 0.041365419657829;
-	const float e = 9.419273457583088E-3;
-	float retval;
-	float intpart;
-	int32_t n;
+    const float a = 1.000000034750736;
+    const float b = 0.499997989964957;
+    const float c = 0.166704077809886;
+    const float d = 0.041365419657829;
+    const float e = 9.419273457583088E-3;
+    float retval;
+    float intpart;
+    int32_t n;
 
     x *= 4;
-	n = (int32_t)x;
-	x -= n;
+    n = (int32_t)x;
+    x -= n;
     x /= 4;
 
-	retval = (((( x * e + d) * x + c) * x + b) * x + a) * x + 1;
-	intpart = power_f(root_e, n);
+    retval = (((( x * e + d) * x + c) * x + b) * x + a) * x + 1;
+    intpart = power_f(root_e, n);
 
-	return intpart * retval;
+    return intpart * retval;
 }
 
 // Calcula e^x
 float exp_f( float x) {
-	if( x < 0) return 1 / _exp_f(-x);
-	return _exp_f(x);
+    if( x < 0) return 1 / _exp_f(-x);
+    return _exp_f(x);
 }
 
 // Calcula la potencia de e^x usando la serie de taylor en orden 5 y
 // reduciendo el rango con m = 1/4, optimizando los coeficientes con minimax
 // (x debe ser siempre mayor que 0)
 static double _exp_lf(double x) {
-	const double root_e = 1.284025416687741;
-	const double a = 1.000000034750736;
-	const double b = 0.499997989964957;
-	const double c = 0.166704077809886;
-	const double d = 0.041365419657829;
-	const double e = 9.419273457583088E-3;
-	double retval;
-	double intpart;
-	int32_t n;
+    const double root_e = 1.284025416687741;
+    const double a = 1.000000034750736;
+    const double b = 0.499997989964957;
+    const double c = 0.166704077809886;
+    const double d = 0.041365419657829;
+    const double e = 9.419273457583088E-3;
+    double retval;
+    double intpart;
+    int32_t n;
 
-	x *= 4;
-	n = (int32_t)x;
-	x -= n;
-	x /= 4;
+    x *= 4;
+    n = (int32_t)x;
+    x -= n;
+    x /= 4;
 
-	retval = (((( x * e + d) * x + c) * x + b) * x + a) * x + 1;
-	intpart = power_lf(root_e, n);
+    retval = (((( x * e + d) * x + c) * x + b) * x + a) * x + 1;
+    intpart = power_lf(root_e, n);
 
-	return intpart * retval;
+    return intpart * retval;
 }
 
 // Calcula e^x
 double exp_lf( double x) {
-	if( x < 0) return 1 / _exp_lf(-x);
-	return _exp_lf(x);
+    if( x < 0) return 1 / _exp_lf(-x);
+    return _exp_lf(x);
 }
 
-// Calcula el logaritmo natural de un punto flotante en el rango 1.587 ... 2
+// Calcula el logaritmo natural de un punto flotante en el rango (0.5)^(1/5) .. (0.5)^(-1/5)
 // Use minimaxed 5th-order rational
 // polynomial to calculate the log.
 static float __ln_f(float x) {
-	const float a = 1;
-	const float b = -0.2672435;
-	const float c = -0.600576;
+    const float a = 1;
+    const float b = -0.2672435;
+    const float c = -0.600576;
 
-	float z = (x - 1) / (x + 1);
-	float z2 = z * z;
-	return 2 * z * (a + b * z2) / (1 + c * z2);
+    float z = (x - 1) / (x + 1);
+    float z2 = z * z;
+    return 2 * z * (a + b * z2) / (1 + c * z2);
 }
 
 // Parte de segmentacion en el cálculo del logaritmo
-// x esta en el rango 1.0 .. 2.0
+// x esta en el rango 0.5 .. 1.0
 static float _ln_f(float x) {
-	const float k1 = 1.2599210498948732;       // 2^(1/3)
-	const float k2 = 1.5874010519681994;       // 2^(2/3)
-	const float ln_k1 = 0.2310490601866484;    // ln(k1)
-	const float ln_k2 = 0.4620981203732968;    // ln(k2)
-
-	if( x >= k2) return __ln_f(x);
-	if( x >= k1) return __ln_f(x * k1) - ln_k1;
-	return __ln_f(x * k2) - ln_k2;
+    const float limit1 = 0.879559563;   // 0.5^(1/5)
+    const float limit2 = 0.659753955;   // 0.5^(3/5)
+    const float k1 = 1.31950791077;     // 0.5^(-2/5)
+    const float k2 = 1.74110112659;     // 0.5^(-4/5)
+    const float ln_k1 = 2.772588722239781e-01;   // ln(k1)
+    const float ln_k2 = 5.545177444479562e-01;   // ln(k2)
+    
+    if( x >= limit1) return __ln_f(x);
+    if( x >= limit2) return __ln_f(x * k1) - ln_k1;
+    return __ln_f(x * k2) - ln_k2;
 }
 
 // Calcula el logaritmo natural de x
 float ln_f(float x) {
-	const float ln2 = 0.69314718; // log(2);
-	int16_t expo;
-	float retval;
-	hack_structure_f _x;
+    const float ln2 = 0.69314718; // log(2);
+    int16_t expo;
+    float retval;
+    hack_structure_f _x;
 
-	if( x <= 0) return -BIG_FLOAT;
-	_x.fp = x;
+    if( x <= 0) return -BIG_FLOAT;
+    _x.fp = x;
 
-	// extraer el exponente
-	expo = ((int32_t)_x.dp & 0x7f800000) >> 23;
-	expo -= 0x7f;
-	// force the exponent to zero
-	_x.dp &= 0x007fffff;
-	_x.dp |= 0x3f800000;
+    // extraer el exponente
+    expo = ((int32_t)_x.dp & 0x7f800000) >> 23;
+    expo -= 0x7e;
+    // force the exponent to -1
+    _x.dp &= 0x007fffff;
+    _x.dp |= 0x3f000000;
 
-	// compute the log of the mantissa only
-	retval = _ln_f(_x.fp);
+    // compute the log of the mantissa only
+    retval = _ln_f(_x.fp);
 
-	// rebuild the result
-	//retval = ln2 * (expo + retval / ln2);
-	retval = ln2 * expo + retval; // ¿porque no mejor esto en lugar de la línea de arriba?
-	return retval;
+    // rebuild the result
+    //retval = ln2 * (expo + retval / ln2);
+    retval = ln2 * expo + retval; // ¿porque no mejor esto en lugar de la línea de arriba?
+    return retval;
 }
 
-// Calcula el logaritmo natural de un punto flotante en el rango 1.587 ... 2
+// Calcula el logaritmo natural de un punto flotante en el rango (0.5)^(1/5) .. (0.5)^(-1/5)
 // Use minimaxed 5th-order rational
 // polynomial to calculate the log.
 static double __ln_lf(double x) {
-	const double a = 1;
-	const double b = -0.2672435;
-	const double c = -0.600576;
+    const double a = 1;
+    const double b = -0.2672435;
+    const double c = -0.600576;
 
-	double z = (x - 1) / (x + 1);
-	double z2 = z * z;
-	return 2 * z * (a + b * z2) / (1 + c * z2);
+    double z = (x - 1) / (x + 1);
+    double z2 = z * z;
+    return 2 * z * (a + b * z2) / (1 + c * z2);
 }
 
 // Parte de segmentacion en el cálculo del logaritmo
-// x esta en el rango 1.0 .. 2.0
+// x esta en el rango 0.5 .. 1.0
 static double _ln_lf(double x) {
-// 	const double limit1 = 0.879559563;	// 0.5^(1/5)
-// 	const double limit2 = 0.659753955;	// 0.5^(3/5)
-// 	const double k1 = 0.757858283;		// 0.5^(2/5)
-// 	const double k2 = 0.574349177;		// 0.5^(4/5)
-// 	const double ln_k = -0.138629436;	// ln(0.5^(1/5))
-// 
-// 	if( x >= limit1) return __ln_lf(x);
-// 	if( x >= limit2) return __ln_lf(x/k1) + 2 * ln_k;
-// 	return __ln_lf(x / k2) + 4 * ln_k;
+    const double limit1 = 0.879559563;    // 0.5^(1/5)
+    const double limit2 = 0.659753955;    // 0.5^(3/5)
+    const double k1 = 0.757858283;    // 0.5^(2/5)
+    const double k2 = 0.574349177;    // 0.5^(4/5)
+    const double ln_k = -0.138629436;    // ln(0.5^(1/5))
     
-    const double k1 = 1.2599210498948732;       // 2^(1/3)
-	const double k2 = 1.5874010519681994;       // 2^(2/3)
-	const double ln_k1 = 0.2310490601866484;    // ln(k1)
-	const double ln_k2 = 0.4620981203732968;    // ln(k2)
-
-	if( x >= k2) return __ln_lf(x);
-	if( x >= k1) return __ln_lf(x * k1) - ln_k1;
-	return __ln_lf(x * k2) - ln_k2;
+    if( x >= limit1) return __ln_lf(x);
+    if( x >= limit2) return __ln_lf(x/k1) + 2 * ln_k;
+    return __ln_lf(x / k2) + 4 * ln_k;
 }
 
 // Calcula el logaritmo natural de x
 double ln_lf(double x) {
-	const double ln2 = 0.69314718; // log(2);
-	int16_t expo;
-	double retval;
-	hack_structure _x;
+    const double ln2 = 0.69314718; // log(2);
+    int16_t expo;
+    double retval;
+    hack_structure_lf _x;
 
-	if( x <= 0) return -BIG_FLOAT;
-	_x.fp = x;
+    if( x <= 0) return -BIG_FLOAT;
+    _x.fp = x;
 
-	// extraer el exponente
-	expo = (int16_t)(_x.hi >> 20);
-	expo -= 0x3fe;
+    // extraer el exponente
+    expo = (int16_t)(_x.hi >> 20);
+    expo -= 0x3fe;
 
-	// force the exponent to zero
-	_x.hi &= 0x000fffff;
-	_x.hi += 0x3fe00000;
+    // force the exponent to -1
+    _x.hi &= 0x000fffff;
+    _x.hi += 0x3fe00000;
 
-	// compute the log of the mantissa only
-	retval = _ln_lf(_x.fp);
+    // compute the log of the mantissa only
+    retval = _ln_lf(_x.fp);
 
-	// rebuild the result
-	//retval = ln2 * (expo + retval / ln2);
-	retval = ln2 * expo + retval; // ¿porque no mejor esto en lugar de la línea de arriba?
-	return retval;
+    // rebuild the result
+    //retval = ln2 * (expo + retval / ln2);
+    retval = ln2 * expo + retval; // ¿porque no mejor esto en lugar de la línea de arriba?
+    return retval;
 }
 
 // Calcula la potencia de un numero cualquiera x a un numero entero n
 float power_f( float x, uint32_t n) {
-	float retval = 1;
-	float factor = x;
+    float retval = 1;
+    float factor = x;
 
-	while( n != 0) {
-		if( (n & 1) != 0) {
-			retval *= factor;
-		}
+    while( n != 0) {
+        if( (n & 1) != 0) {
+            retval *= factor;
+        }
 
-		factor *= factor;
-		n >>= 1;
-	}
-	return retval;
+        factor *= factor;
+        n >>= 1;
+    }
+    return retval;
 }
 
 float pow_f(float x, float y) {
@@ -215,18 +208,18 @@ float pow_f(float x, float y) {
 
 // Calcula la potencia de un numero cualquiera x a un numero entero n
 double power_lf( double x, uint32_t n) {
-	double retval = 1;
-	double factor = x;
+    double retval = 1;
+    double factor = x;
 
-	while( n != 0) {
-		if( (n & 1) != 0) {
-			retval *= factor;
-		}
+    while( n != 0) {
+        if( (n & 1) != 0) {
+            retval *= factor;
+        }
 
-		factor *= factor;
-		n >>= 1;
-	}
-	return retval;
+        factor *= factor;
+        n >>= 1;
+    }
+    return retval;
 }
 
 // Calcula la potencia x^y
@@ -238,7 +231,4 @@ double pow_lf(double x, double y) {
     if( (int64_t)y % 2 == 0) return exp_lf(y * ln_lf(-x));
     return -exp_lf(y * ln_lf(-x));
 }
-
-
-
 
--- src/math_tool/math_tool.h
+++ src/math_tool/math_tool.h
@@ -5,28 +5,61 @@
 
 #define BIG_FLOAT 1.0e30;
 
-// Union para hckear el exponente de un punto flotante tipo double
+// Union para hackear el exponente de un punto flotante tipo double
+// According to IEEE 754 standard, the floating-point number is represented in following ways:
+//     Half Precision (16 bit): 1 sign bit, 5 bit exponent, and 10 bit mantissa
+//     Single Precision (32 bit): 1 sign bit, 8 bit exponent, and 23 bit mantissa
+//     Double Precision (64 bit): 1 sign bit, 11 bit exponent, and 52 bit mantissa
+//     Quadruple Precision (128 bit): 1 sign bit, 15 bit exponent, and 112 bit mantissa
+//
+// Single Precision:
+//  float 32bit IEEE 754
+//  +---+------------------------+-------------------------------------------------------------------------------+
+//  | s |           e            |                                        m                                      |
+//  +---+------------------------+-------------------------------------------------------------------------------+
+//   31  30                    24 23                                                                            0
+//  s = signo        1 bit
+//  e = exponente    8 bits
+//  m = mantisa      23 bits
+//
+//     The Exp field gives the exponent of the number, as a power of two. It is biased by 0x7F (127), so that very 
+//     small numbers have exponents near zero and very large numbers have exponents near 0xFF (255).
+//
+//     If Exp = 0x7D (125), the number is between 0.25 and 0.5 (not including 0.5).
+//     If Exp = 0x7E (126), the number is between 0.5 and 1.0 (not including 1.0).
+//     If Exp = 0x7F (127), the number is between 1.0 and 2.0 (not including 2.0).
+//     If Exp = 0x80 (128), the number is between 2.0 and 4.0 (not including 4.0).
+//     If Exp = 0x81 (129), the number is between 4.0 and 8.0 (not including 8.0).
+//
+// Ejemplos
+//     Float value    S     Exp     Frac         Mathematical value
+//     0x3F800000     0     0x7F    000...000    1.0
+//     0xBF800000     1     0x7F    000...000    -1.0
+//     0x3F800001     0     0x7F    000...001    1.000 000 119
+//     0x3F400000     0     0x7E    100...000    0.75
+//     0x00800000     0     0x01    000...000    1.18*10^-38
+//     0x00000001     0     0x00    000...001    1.40*10^-45
+//     0x7F7FFFFF     0     0xFE    111...111    3.40*10^38
+//     0x7F800000     0     0xFF    000...000    Plus infinity
+//     0xFF800000     1     0xFF    000...000    Minus infinity
+//     0x00000000     0     0x00    000...000    0.0
+//     0x7F800001     0     0xFF    000...001    Signaling NaN
+//     0x7FC00000     0     0xFF    100...000    Quiet NaN
+//
 typedef union {
-	double fp;
-	struct {
-		uint32_t lo;
-		uint32_t hi;
-	};
-} hack_structure;
-
-
-// float 32bit IEEE 754
-// +---+------------------------+-------------------------------------------------------------------------------+
-// | s |           e            |                                        m                                      |
-// +---+------------------------+-------------------------------------------------------------------------------+
-//  31  30                    24 23                                                                            0
-// s = signo        1 bit
-// e = exponente    8 bits
-// m = mantisa      23 bits
-typedef union {
-	float fp;
+    float fp;
     uint32_t dp;
 } hack_structure_f;
+// Double Prevision:
+//     The Exp field is biased by 0x3FF (1023) instead of 0x7F, so numbers between 1.0 and 2.0 have an Exp field of 0x3FF.
+//     The Exp value representing infinity and NaNs is 0x7FF (2047) instead of 0xFF.
+typedef union {
+    double fp;
+    struct {
+        uint32_t lo;
+        uint32_t hi;
+    };
+} hack_structure_lf;
 
 // Exponencial
 float exp_f(float x);
